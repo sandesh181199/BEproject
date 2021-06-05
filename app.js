985f7da46de47a51146eae6d71fbbc26ea6b6f48
Aayush Gattawar <40913123+aayush1gattawar@users.noreply.github.com>
2021-06-05 13:17:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-var express = require('express');
-var config = require('./config');
-const http = require('http');
-var mongoose = require('mongoose');
+var express = require("express");
+var config = require("./config");
+const http = require("http");
+var mongoose = require("mongoose");
 var app = express();
 
-var index = require('./routes/index');
+var index = require("./routes/index");
 
 const url = config.mongoUrl;
-const connect = mongoose.connect(url, {useNewUrlParser : true, useUnifiedTopology : true});
+const connect = mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-connect.then((db)=>{
-  console.log('Sucessfully Connected to the server');
-}, (err) =>{
-  console.log(err);
-})
+connect.then(
+  (db) => {
+    console.log("Sucessfully Connected to the server");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
 
-const hostname='localhost';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use('/', index.router);
-
+app.use("/", index.router);
 
 const server = http.createServer(app);
-app.listen(port,hostname,()=>{
-    console.log(`server running on http://${hostname}:${port}`)
-})
+app.listen(port, () => {
+  console.log(`server running on ${port}`);
+});
