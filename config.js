bd39b4b3980a6c2487052f1abc83b9ced62cce21
Aayush Gattawar <40913123+aayush1gattawar@users.noreply.github.com>
2021-10-23 08:20:11 +0530

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    'secretKey' : '1234-5678',
-    'mongoUrl' : 'mongodb+srv://root1234:root1234@progang-cluster0.1anwp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
